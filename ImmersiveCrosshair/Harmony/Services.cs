7080c485b78e5bdf1c2c3bcd3a44ecc7841acf9c
Rizart Dokollari <r.dokollari@gmail.com>
2024-09-11 23:24:00 +0100

    chore: Services injector.

---
+++ ImmersiveCrosshair/Harmony/Services.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+namespace ImmersiveCrosshair.Harmony
+{
+    public static class Services
+    {
+        private static readonly Dictionary<Type, Delegate> Database =
+            new Dictionary<Type, Delegate>();
+
+        private static void Add<TService>(Func<object[], TService> provider)
+        {
+            Database[typeof(TService)] = provider;
+        }
+
+        public static TService Get<TService>(params object[] args)
+        {
+            if (Database.TryGetValue(typeof(TService), out var provider))
+            {
+                return ((Func<object[], TService>)provider)(args);
+            }
+
+            throw new Exception($"Service {typeof(TService)} not registered");
+        }
+
+        public static void Initialise()
+        {
+            var logger = new Logger();
+            var modSettings = new ModSettings(logger);
+
+            Add<IGuiDrawCrosshair>(args => new GuiDrawCrosshair(logger, modSettings));
+        }
+    }
+}

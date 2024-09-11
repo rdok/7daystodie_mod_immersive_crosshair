b3884dc86dafc23e727a1365143e886c30fc88b2
Rizart Dokollari <r.dokollari@gmail.com>
2024-09-11 23:24:35 +0100

    feat: Load mod settings from xml settings file.

---
+++ ImmersiveCrosshair/Harmony/ModSettings.cs
@@ -0,0 +1,86 @@
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace ImmersiveCrosshair.Harmony
+{
+    public class ModSettings : IModSettings
+    {
+        private readonly ILogger _logger;
+        private XDocument _settingsXml;
+        public string ToolsSetting { get; set; }
+        public string MeleeWeaponsSetting { get; set; }
+        public string BowsSetting { get; set; }
+        public string RangedWeaponsSetting { get; set; }
+
+        public ModSettings(Logger logger)
+        {
+            _logger = logger;
+
+            MeleeWeaponsSetting = GetString("General", "ImmersiveCrosshair", "MeleeWeapons");
+            BowsSetting = GetString("General", "ImmersiveCrosshair", "Bows");
+            RangedWeaponsSetting = GetString("General", "ImmersiveCrosshair", "RangedWeapons");
+            ToolsSetting = GetString("General", "ImmersiveCrosshair", "Tools");
+        }
+
+
+        private void EnsureSettingsLoaded()
+        {
+            if (_settingsXml != null) return;
+
+            var modPath = Assembly.GetExecutingAssembly().Location;
+            var modDirectory = Path.GetDirectoryName(modPath);
+            var xmlPath = Path.Combine(modDirectory, "ModSettings.xml");
+            _settingsXml = XDocument.Load(xmlPath);
+
+            _logger.Debug($"Settings loaded from: {xmlPath}");
+        }
+
+        private string GetSettingValue(string tab, string category, string settingName)
+        {
+            EnsureSettingsLoaded();
+            var value = _settingsXml.Element("ModSettings")
+                ?.Element("Global")
+                ?.Elements("Tab")
+                .FirstOrDefault(e => e.Attribute("name")?.Value == tab)
+                ?.Elements("Category")
+                .FirstOrDefault(e => e.Attribute("name")?.Value == category)
+                ?.Elements()
+                .FirstOrDefault(e => e.Attribute("name")?.Value == settingName)
+                ?.Attribute("defaultValue")
+                ?.Value;
+
+            _logger.Debug($"{tab}.{category}.{settingName}: {value}");
+            return value;
+        }
+
+        public float GetFloat(string tab, string category, string settingName)
+        {
+            var value = float.Parse(GetSettingValue(tab, category, settingName));
+            _logger.Debug($"{tab}.{category}.{settingName}: {value}");
+            return value;
+        }
+
+        public byte GetByte(string tab, string category, string settingName)
+        {
+            var value = byte.Parse(GetSettingValue(tab, category, settingName));
+            _logger.Debug($"{tab}.{category}.{settingName}: {value}");
+            return value;
+        }
+
+        public bool GetBool(string tab, string category, string settingName)
+        {
+            var value = bool.Parse(GetSettingValue(tab, category, settingName));
+            _logger.Debug($"{tab}.{category}.{settingName}: {value}");
+            return value;
+        }
+
+        public string GetString(string tab, string category, string settingName)
+        {
+            var value = GetSettingValue(tab, category, settingName);
+            _logger.Debug($"{tab}.{category}.{settingName}: {value}");
+            return value;
+        }
+    }
+}

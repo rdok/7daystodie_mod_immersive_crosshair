0d5b691a76eeedcdceddf0c47c3fa4eeb3c91c77
Rizart Dokollari <r.dokollari@gmail.com>
2024-09-11 23:27:14 +0100

    test: it enables the crosshair having interaction prompt opened

--- UnitTests/Harmony/InteractionPromptTests.cs
+++ UnitTests/Harmony/InteractionPromptTests.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using Moq;
 using NUnit.Framework;
@@ -11,26 +10,11 @@ public class InteractionPromptTests
     [Test]
     public void it_enables_the_crosshair_having_interaction_prompt_opened()
     {
-        var (playerLocalMock, hudMock) = Factory.Create(new Dictionary<string, object>(Factory.Input)
-            {
-                ["HasInteractionPromptOpened"] = true,
-            }
-        );
-        ImmersiveCrosshair.Harmony.ImmersiveCrosshair.ApplyPatch(playerLocalMock.Object);
+        var (guiDrawCrosshair, entityPlayerLocalMock, itemActionMock, worldRayHitInfoMock, hudMock) =
+            Factory.Create(new Dictionary<string, object>(Factory.Input) { ["HasInteractionPromptOpened"] = true });
+
+        guiDrawCrosshair.Update(entityPlayerLocalMock.Object, itemActionMock.Object, worldRayHitInfoMock.Object);
+
         hudMock.VerifySet(h => h.showCrosshair = true, Times.Once);
-    }
-
-
-    [Test]
-    public void it_does_not_modify_the_crosshair_having_interaction_closed()
-    {
-        var (playerLocalMock, hudMock) = Factory.Create(new Dictionary<string, object>(Factory.Input)
-            {
-                ["HasInteractionPromptOpened"] = false,
-                ["HitInfo"] = false,
-            }
-        );
-        ImmersiveCrosshair.Harmony.ImmersiveCrosshair.ApplyPatch(playerLocalMock.Object);
-        hudMock.VerifyNoOtherCalls();
     }
 }
